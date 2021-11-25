153e76d6d768355ce8397ecceb44db70bdc130a1
Lucas Maieski <lucmaieski@gmail.com>
2021-11-24 23:30:42 -0300

    Feito requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,38 +1,67 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
-const TABLE_CATEGORIES = ['Descrição', 'Tag', 'Método de pagamento', 'Valor',
-  'Moeda', 'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão',
-  'Editar/Excluir'];
-
 class Table extends React.Component {
-  constructor() {
-    super();
-
-    this.tableTitles = this.tableTitles.bind(this);
-  }
-
-  tableTitles() {
-    return TABLE_CATEGORIES.map((cat) => (
-      <th key={ cat }>{cat}</th>
-    ));
-  }
-
   render() {
+    const { expenses } = this.props;
+
     return (
       <div>
         <table>
-          <tr>
-            {this.tableTitles()}
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+            </tr>
+          </thead>
+          <tbody>
+            { expenses.map((exp) => {
+              const { description, tag, method, value, currency, exchangeRates } = exp;
+              const { ask } = exchangeRates[currency];
+              const convertedValue = ask * value;
+
+              return (
+                <tr key={ value }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{value}</td>
+                  <td>
+                    {
+                      currency === 'USD' ? 'Dólar Comercial'
+                        : exchangeRates[currency].name
+                    }
+                  </td>
+                  <td>{ parseFloat(exchangeRates[currency].ask).toFixed(2) }</td>
+                  <td>
+                    {convertedValue.toFixed(2)}
+                  </td>
+                  <td>Real</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
+Table.propTypes = {
+  expenses: PropTypes.shape({
+    map: PropTypes.func,
+  }).isRequired,
+};
+
 const mapStateToProps = (state) => ({
-  tag: state.wallet.expenses,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps)(Table);
